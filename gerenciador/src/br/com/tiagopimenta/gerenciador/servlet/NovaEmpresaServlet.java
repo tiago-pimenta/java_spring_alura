19bde6a320074fe461bda15319d0ccacd03b0373
Tiago Pimenta <tiagopimenta@gmail.com>
2021-03-18 22:53:41 -0300

    a

--- gerenciador/src/br/com/tiagopimenta/gerenciador/servlet/NovaEmpresaServlet.java
+++ gerenciador/src/br/com/tiagopimenta/gerenciador/servlet/NovaEmpresaServlet.java
@@ -17,10 +17,16 @@ public class NovaEmpresaServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Cadastrando Nova Empresa");
 		
 		String nomeEmpresa = request.getParameter("nome");
+		
+		Empresa empresa = new Empresa();
+		empresa.setNome(nomeEmpresa);
+		
+		Banco banco = new Banco();
+		banco.adiciona(empresa);
 		
 		PrintWriter out = response.getWriter();
 		
