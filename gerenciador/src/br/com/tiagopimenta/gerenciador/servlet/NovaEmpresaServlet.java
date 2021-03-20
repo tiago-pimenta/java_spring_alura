e272ce1453de9946cdbde97f55bc7af305a1d77a
Tiago Pimenta <tiagopimenta@gmail.com>
2021-03-20 15:58:52 -0300

    a

--- gerenciador/src/br/com/tiagopimenta/gerenciador/servlet/NovaEmpresaServlet.java
+++ gerenciador/src/br/com/tiagopimenta/gerenciador/servlet/NovaEmpresaServlet.java
@@ -3,6 +3,7 @@ package br.com.tiagopimenta.gerenciador.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -28,9 +29,11 @@ public class NovaEmpresaServlet extends HttpServlet {
 		Banco banco = new Banco();
 		banco.adiciona(empresa);
 		
-		PrintWriter out = response.getWriter();
-		
-		out.println("<html><body>Empresa " + nomeEmpresa + " cadastrada com sucesso !</body></html>");
+		//Chamar o JSP
+		RequestDispatcher rd = request.getRequestDispatcher("/novaEmpresaCriada.jsp");
+		request.setAttribute("empresa", empresa.getNome());
+		rd.forward(request, response);
+
 	}
 
 }
