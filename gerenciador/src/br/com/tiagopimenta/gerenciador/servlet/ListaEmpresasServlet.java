668174fed89c4412af97a29e631e0db1b30be9b4
Tiago Pimenta <tiagopimenta@gmail.com>
2021-03-21 23:09:59 -0300

    a

--- gerenciador/src/br/com/tiagopimenta/gerenciador/servlet/ListaEmpresasServlet.java
+++ gerenciador/src/br/com/tiagopimenta/gerenciador/servlet/ListaEmpresasServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -22,19 +23,10 @@ public class ListaEmpresasServlet extends HttpServlet {
 		Banco banco = new Banco();
 		List<Empresa> lista = banco.getEmpresas();
 		
-		PrintWriter out = response.getWriter();
+		request.setAttribute("empresas", lista);
 		
-		out.println("<html>");
-		out.println("<body>");
-		out.println("<ul>");
-		
-		for (Empresa empresa : lista) {
-			out.println("<li>" + empresa.getNome() + "</li>");
-		}
-		
-		out.println("</ul>");
-		out.println("</body>");
-		out.println("</html>");
+		RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas.jsp");
+		rd.forward(request, response);
 
 	}
 
